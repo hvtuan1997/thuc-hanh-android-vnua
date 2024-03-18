71580f64e5b1ccb104f79acfb20fabe73c41aaa7
Hoang Van Tuan <tuanhoang.97dx@gmail.com>
2024-03-18 10:44:05 +0700

    Up code LinearLayout, TableLayout

---
+++ Code_tham_khao/Buoi_1/ND_4_Layout/app/src/main/java/com/example/nd_4_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nd_4_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.learn_tablelayout);
+    }
+}
